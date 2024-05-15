28c0b36f48454e1b89836d6bbb44bbdc106ba710
Arleny <arleny.ares@gmail.com>
2024-05-16 00:36:09 +0200

    autoplay carousel component without images

--- filmLix/src/App.vue
+++ filmLix/src/App.vue
@@ -1,23 +1,14 @@
-<script >
+<script setup >
+import Carrusel from "./components/Carrusel.vue"
+
 
 </script>
 
 <template>
-  <header>
-    
-    <nav class="flex bg-customGray">
-      <div>
-        <img src="./assets/filmlix_logo.png" alt="">
-        <h2 class="uppercase text-customWhite">filmlix</h2>
-      </div>
-      <ul class="">
-        <li class="myLi">My movies</li>
-        <li class="myLi">Genres</li>
-        <li class="myLi">Lists</li>
-        <li class="myLi">Random</li>
-      </ul>
-    </nav>
-  </header>
+  
+    <body> 
+        <carrusel/>
+    </body>
 </template>
 
 <style >
