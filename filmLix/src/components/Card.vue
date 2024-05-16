7056cf32c24d8414114089ff1cf8bc4917a40a23
Evgenii Igoshin <johnshin378@gmail.com>
2024-05-16 14:50:51 +0200

    Card added

---
+++ filmLix/src/components/Card.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+   <div class="card">
+      <a href=""><img src="../assets/img/pic.jpg" class="rounded-lg hover:translate-x-1 hover:-translate-y-1 duration-200 w-full" alt=""></a>
+      <div class="w-full h-12 flex justify-around items-center text-xl">
+         <a href="" ><i class="fa-regular fa-eye cardIcon"></i></a>
+         <a href=""><i class="fa-regular fa-heart cardIcon"></i></a>
+         <a href=""><i class="fa-regular fa-bookmark cardIcon"></i></a>
+      </div>
+   </div>
+</template>
+
+
+<script>
+
+</script>
+
