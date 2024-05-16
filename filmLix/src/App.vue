ddc74144a8ab70932edfffa21c5fd70b7a9eb403
01joanna <joa.cuevas9@gmail.com>
2024-05-16 10:08:21 +0200

    no loonger header wip

--- filmLix/src/App.vue
+++ filmLix/src/App.vue
@@ -4,19 +4,6 @@
 
 <template>
   <header>
-    
-    <nav class="flex bg-customGray">
-      <div>
-        <img src="./assets/filmlix_logo.png" alt="">
-        <h2 class="uppercase text-customWhite">filmlix</h2>
-      </div>
-      <ul class="">
-        <li class="myLi">My movies</li>
-        <li class="myLi">Genres</li>
-        <li class="myLi">Lists</li>
-        <li class="myLi">Random</li>
-      </ul>
-    </nav>
   </header>
 </template>
 
