0aa4566bab2e3bc8eb871deb2091de0e42761c97
01joanna <joa.cuevas9@gmail.com>
2024-05-20 12:54:35 +0200

    single page structure created

---
+++ filmLix/src/views/SingleMovie.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <div class="singleCard"></div>
+        <div class="cardDescription text-white">
+            <div class="title flex flex-col gap-2">
+                <h2 class="text-[60px] font-bold">Lost in Translation</h2>
+                <div class="flex gap-10 text-sm">
+                    <p>2003</p>
+                    <p>Directed by Sofia Coppola</p>
+                </div>
+            </div>
+            <div class="description flex flex-col gap-8">
+                <p>Two lost souls visiting Tokyo – the young, neglected wife of a photographer and a washed-up movie star shooting a TV commercial – find an odd solace and pensive freedom to be real in each other’s company, away from their lives in America.</p>
+                <p class="text-xs">102 minutes</p>
+            </div>
+            <div class="movieDetails">
+                <div class="movieGenre flex flex-col gap-10">
+                    <p class="font-bold">Genres</p>
+                    <p>Comedy, Drama</p>
+                </div>
+                <div class="movieActors flex flex-col gap-10">
+                    <p class="font-bold">Actors</p>
+                    <p>Bill Murray</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script></script>
+<style scoped></style>
