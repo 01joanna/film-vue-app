3333195534c3a345566b9448f224022da38d91cd
01joanna <joa.cuevas9@gmail.com>
2024-05-21 12:10:50 +0200

    changed styles

--- filmLix/src/components/Card.vue
+++ filmLix/src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="card" v-if="movie">
       <router-link :to="{ name: 'MovieDetails', params: { id: movie.id } }">
-         <img :src="movie.Poster" class="rounded-lg hover:translate-x-1 hover:-translate-y-1 duration-200 w-full h-[85%]" alt="">
+         <img :src="movie.Poster" class="rounded-lg hover:translate-x-1 hover:-translate-y-1 duration-200 w-[300px] h-[85%]" alt="">
       </router-link>
       <div class="w-full h-12 flex justify-around items-center text-xl">
          <a href=""><i class="fa-regular fa-eye cardIcon text-customBlack"></i></a>
