172cad18f905cbc286fcb4b96d81d204dc901ed9
01joanna <joa.cuevas9@gmail.com>
2024-05-27 12:13:45 +0200

    alerts on cards, changes color when needed

--- filmLix/src/components/Card.vue
+++ filmLix/src/components/Card.vue
@@ -5,15 +5,22 @@
             class="rounded-lg hover:translate-x-1 hover:-translate-y-1 duration-200 w-[300px] h-[85%]" alt="">
       </router-link>
       <div class="w-full h-12 flex justify-around items-center text-xl">
-         <a @click.prevent="addToWatchedMovies"><i class="fa-regular fa-eye cardIcon text-customBlack"></i></a>
-         <a @click.prevent="addToFavoriteMovies"><i class="fa-regular fa-heart cardIcon text-customBlack"></i></a>
-         <a @click.prevent="addToWatchlistMovies"><i class="fa-regular fa-bookmark cardIcon text-customBlack"></i></a>
+         <a @click.prevent="addToWatchedMovies">
+            <i class="fa-regular fa-eye cardIcon" :style="{ color: isWatched ? 'green' : 'black' }"></i>
+         </a>
+         <a @click.prevent="addToFavoriteMovies">
+            <i class="fa-regular fa-heart cardIcon" :style="{ color: isFavorite ? 'red' : 'black' }"></i>
+         </a>
+         <a @click.prevent="addToWatchlistMovies">
+            <i class="fa-regular fa-bookmark cardIcon" :style="{ color: isInWatchlist ? 'blue' : 'black' }"></i>
+         </a>
       </div>
    </div>
 </template>
 
 <script>
 import { useMoviesStore } from '../stores/movies.js';
+import { computed } from 'vue';
 
 export default {
    props: {
@@ -22,22 +29,32 @@ export default {
    setup(props) {
       const store = useMoviesStore();
 
+      const isWatched = computed(() => store.isWatched(props.movie));
+      const isFavorite = computed(() => store.isFavorite(props.movie));
+      const isInWatchlist = computed(() => store.isInWatchlist(props.movie));
+
       const addToWatchedMovies = () => {
          store.addWatchedMovie(props.movie);
+         alert('Film added to your watched movies list');
       };
 
       const addToFavoriteMovies = () => {
          store.addFavoriteMovie(props.movie);
+         alert('Film added to your favorite movies list');
       };
 
       const addToWatchlistMovies = () => {
          store.addWatchlistMovie(props.movie);
+         alert('Film added to your watchlist movies list');
       };
 
       return {
          addToWatchedMovies,
          addToFavoriteMovies,
-         addToWatchlistMovies
+         addToWatchlistMovies,
+         isWatched,
+         isFavorite,
+         isInWatchlist
       };
    }
 };
--- filmLix/src/stores/movies.js
+++ filmLix/src/stores/movies.js
@@ -1,27 +1,48 @@
-
 import { defineStore } from "pinia";
 
 export const useMoviesStore = defineStore("movies", {
     state: () => ({
-        watchedMovies: [],
-        favoriteMovies: [],
-        watchlistMovies: [],
+        watchedMovies: JSON.parse(localStorage.getItem('watchedMovies')) || [],
+        favoriteMovies: JSON.parse(localStorage.getItem('favoriteMovies')) || [],
+        watchlistMovies: JSON.parse(localStorage.getItem('watchlistMovies')) || [],
     }),
     actions: {
         addWatchedMovie(movie) {
             if (!this.watchedMovies.find((m) => m.id === movie.id)) {
                 this.watchedMovies.push(movie);
+                this.removeFromWatchlist(movie);
+                this.saveToLocalStorage();
             }
         },
         addFavoriteMovie(movie) {
             if (!this.favoriteMovies.find((m) => m.id === movie.id)) {
                 this.favoriteMovies.push(movie);
+                this.saveToLocalStorage();
             }
         },
         addWatchlistMovie(movie) {
             if (!this.watchlistMovies.find((m) => m.id === movie.id)) {
                 this.watchlistMovies.push(movie);
+                this.saveToLocalStorage();
             }
         },
+        saveToLocalStorage() {
+            localStorage.setItem('watchedMovies', JSON.stringify(this.watchedMovies));
+            localStorage.setItem('favoriteMovies', JSON.stringify(this.favoriteMovies));
+            localStorage.setItem('watchlistMovies', JSON.stringify(this.watchlistMovies));
+        },
+        removeFromWatchlist(movie) {
+            this.watchlistMovies = this.watchlistMovies.filter((m) => m.id !== movie.id);
+            this.saveToLocalStorage();
+        },
+        isWatched(movie) {
+            return this.watchedMovies.some((m) => m.id === movie.id);
+        },
+        isFavorite(movie) {
+            return this.favoriteMovies.some((m) => m.id === movie.id);
+        },
+        isInWatchlist(movie) {
+            return this.watchlistMovies.some((m) => m.id === movie.id);
+        }
     },
 });
