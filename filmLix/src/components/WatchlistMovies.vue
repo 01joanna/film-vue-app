d4da6a30f4ae67ab4b8b94f599b3ddb736f1ef50
CeciliaB22 <ceciliabalaban11@gmail.com>
2024-05-28 10:17:31 +0200

    deleted index from wachedmovies and watchlistmovies

--- filmLix/src/components/WatchlistMovies.vue
+++ filmLix/src/components/WatchlistMovies.vue
@@ -8,7 +8,7 @@
         </div>
         <section class="secondHomepage flex flex-col gap-5 mb-20 justify-center items-center">
             <div class="grid lg:grid-cols-4 gap-10 md:grid-cols-2" v-if="watchlistMovies.length > 0">
-                <Card v-for="(movie, index) in watchlistMovies.slice(0, watchlistMoviesCount)" :key="movie.id"
+                <Card v-for="(movie) in watchlistMovies.slice(0, watchlistMoviesCount)" :key="movie.id"
                     :movie="movie" />
             </div>
             <p v-else class="text-xl text-customWhite">You still haven't added any movies to the watchlist</p>
